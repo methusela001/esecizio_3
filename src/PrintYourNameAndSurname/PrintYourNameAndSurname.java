97bf46d98e572b8df1fe4b1e01bf16396065eac1
Methusela <nethusela777@gmail.com>
2023-07-12 17:21:17 +0200

    3 esercizio

---
+++ src/PrintYourNameAndSurname/PrintYourNameAndSurname.java
@@ -0,0 +1,9 @@
+package PrintYourNameAndSurname;
+
+public class PrintYourNameAndSurname {
+    public static void main(String[] args) {
+        System.out.print("Methusela");
+        System.out.print("     Fenando");
+
+    }
+}
